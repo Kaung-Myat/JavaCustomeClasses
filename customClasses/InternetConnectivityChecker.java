977d66abd15f5463535b558eed10db0e07a1ed16
Kaung Myat Thu <kaungmyatthu5790@gmail.com>
2023-09-20 00:20:41 -0700

    remove main method

--- customClasses/InternetConnectivityChecker.java
+++ customClasses/InternetConnectivityChecker.java
@@ -8,14 +8,6 @@ import java.net.UnknownHostException;
 
 public class InternetConnectivityChecker {
 	
-	public static void main(String[] args) {
-        if (isInternetConnected()) {
-            System.out.println("Internet is connected.");
-        } else {
-            System.out.println("Internet is not connected.");
-        }
-    }
-	
 	public static boolean isInternetConnected() {
 		try {
                InetAddress address = InetAddress.getByName("8.8.8.8");
